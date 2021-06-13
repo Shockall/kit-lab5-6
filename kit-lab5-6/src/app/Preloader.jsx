d7fb0e8559bc7ddaf511c353b7c97e0460520e3f
Shockall <65356100+Shockall@users.noreply.github.com>
2021-06-13 18:30:21 +0300

    Add files via upload

---
+++ kit-lab5-6/src/app/Preloader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Preloader extends React.Component {
+  render() {
+    return (
+      <div id="preloader">
+        <img
+          src="https://tenor.com/xVgl.gif"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
